182967a285a2a902f674360ff9f9b85a23f7860a
LeonCicak <leoncicak07@gmail.com>
2024-10-12 12:33:43 +0200

    Add files via upload

---
+++ Vjezba4/Vjezba4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vjezba4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite broj: ");
+            int broj = int.Parse(Console.ReadLine());
+
+            if (broj % 4 == 0 && broj % 6 == 0)
+            {
+                Console.WriteLine("Broj je djeljiv sa 4 i sa 6");
+            }
+            else if (broj % 4 == 0)
+            {
+                Console.WriteLine("Broj je djeljiv s 4");
+            }
+            else if (broj % 6 == 0)
+            {
+                Console.WriteLine("Broj je djeljiv s 6");
+            }
+            else
+            {
+                Console.WriteLine("Broj nije djeljiv sa 4 ni sa 6");
+            }
+        }
+    }
+}
